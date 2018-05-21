604ea2cef6db620e07560573e2aac9e4549fa358
Athul Muralidharan <athulmurali@gmail.com>
2018-05-21 02:00:25 -0400

    Added user registration module fixes
    Success :
    - User creation if the data is valid and not duplicate
    To do :
    Validation
    Error code mapping
    Swap the form with  a  success banner on success
    Show validation errors on failure in respective text boxes.

--- src/main/webapp/jquery/components/user-register/user-register-controller.js
+++ src/main/webapp/jquery/components/user-register/user-register-controller.js
@@ -7,22 +7,57 @@ function register(){
     if (formValidated){
         console.log($('#dob').val());
         console.log("registering");
+        var user = getUserDetailsFromForm();
+        console.log(user);
+
+        var userService = new UserServiceClient();
+        userService.createUser(user);
+
     }
 
 }
 
+
+function getUserDetailsFromForm() {
+    const userName = $('#username').val();
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const email = $('#email').val();
+    const password = $('#password').val();
+    const confirmPassword = $('#confirmPassword').val();
+    const role = $('#role').val();
+    const dob = $('#dob').val();
+
+    console.log("printing user details");
+    console.log(userName + firstName + lastName + email + password + confirmPassword + role + dob);
+    return {
+        username : userName,
+        email : email,
+        firstName : firstName,
+        lastName : lastName,
+        password : password,
+        confirmPassword : confirmPassword,
+        role: role,
+        dob : dob
+    }
+
+}
+
+function main() {
+    $('#register').click(register);
+
+    // var user = {
+    //     username    : "username",
+    //     firstName   : "First",
+
+}
 //IIFE
 (function () {
 
     jQuery(main);
 
-
     var template;
 
-
-    function main() {
-        $('#register').click(register);
-    }
 
 
 })();
