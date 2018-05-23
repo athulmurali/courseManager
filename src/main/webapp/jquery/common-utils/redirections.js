adbe4f88dda6b3fe8e0860f2fa8556db397602bc
Athul Muralidharan <athulmurali@gmail.com>
2018-05-23 19:58:11 -0400

    Added redirection urls into common utils for login and logout

--- src/main/webapp/jquery/common-utils/redirections.js
+++ src/main/webapp/jquery/common-utils/redirections.js
@@ -1,6 +1,11 @@
 function redirectToProfile(){
     window.location.href= "/jquery/components/profile/profile.template.client.html";
+
 }
 function redirectToRegister(){
     window.location.href="/jquery/components/user-register/user-register-template.html";
+}
+
+function redirectToLogin(){
+    window.location.href="/jquery/components/user-login/user-login.html";
 }
