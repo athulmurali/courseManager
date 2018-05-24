fd019564845eb6f32248379ac5bdd1e33290e44b
Athul Muralidharan <athulmurali@gmail.com>
2018-05-24 15:49:17 -0400

    Added validation for register screen
    From now on the register button will be disabled if the form validation fails.

--- src/main/webapp/jquery/components/user-register/user-register-controller.js
+++ src/main/webapp/jquery/components/user-register/user-register-controller.js
@@ -1,8 +1,123 @@
 const button = document.querySelector('.btn')
 const form   = document.querySelector('.form')
-const formValidated = true;
 
+
+
+const formValidated = true;
 var emailValidated = false;
+
+/**Enables and disables buttons based on all validation events in form**/
+
+function registerOnFormChange()
+{
+    const formValidated = ifFormValid();
+    console.log(formValidated);
+
+    if (formValidated){
+        $("#register").prop("disabled",false);
+    }
+    else{
+        $("#register").prop("disabled",true);
+    }
+    return;
+
+}
+
+function ifFormValid() {
+    return  validateUserName() &&
+        emailValidated             &&
+        isPasswordLengthValid()         &&
+        isPasswordConfirmed()&&
+        validatePhoneNumber();
+}
+
+
+function isPasswordLengthValid()
+{
+    if ($('#password').val().length < 9 ||  $('#password').val().length >20)
+    {
+        $('#passwordHelp').css("display", "block");
+        return false;
+    }
+
+    else
+    {
+        $('#passwordHelp').css("display", "none");
+        return true;
+    }
+}
+
+function isPasswordConfirmed(){
+
+    return ($('#password').val() == $('#confirmPassword').val() &&
+        $('#password').val()!= ''); }
+
+
+function validateUserName() {
+
+    const username = $('#username').val();
+    const usernameRegex = /^[a-zA-Z0-9]+$/;
+    if (usernameRegex.test(String(username).toLowerCase())){
+        $('#userNameHelp').hide();
+        return true;
+    }
+    else{
+        $('#userNameHelp').show();
+        return false;
+
+    }
+}
+
+
+function validatePhoneNumber(){
+        var phoneNumberPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+        console.log($('#phoneNo').val());
+       if (phoneNumberPattern.test($('#phoneNo').val())){
+           $('#phoneNoHelp').hide()
+           return true;
+
+       }
+       else{
+           $('#phoneNoHelp').show()
+           return false;
+
+       }
+
+}
+
+function validateDOB(){
+    console.log( new Date($('#dob').val()))
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function emailValidate() {
+    var $emailAvailable = $("#emailAvailable");
+    var email = $("#email").val();
+
+    if (validateEmail(email))
+    {
+        emailValidated = true;
+        console.log("existing email check : ", emailValidated);
+        checkIfEmailNotTaken(email);
+        registerOnFormChange()
+        return emailValidated;
+    }
+    else {
+
+        $emailAvailable.text("invalid");
+        $emailAvailable.css("color", "red");
+        registerOnFormChange()
+
+        return false;
+    }
+}
+
+
 
 function register(){
     if (formValidated){
@@ -41,10 +156,11 @@ function getUserDetailsFromForm() {
     const password = $('#password').val();
     const confirmPassword = $('#confirmPassword').val();
     const role = $('#role').val();
+    const phone = $('#phoneNo').val()
     const dob = $('#dob').val();
 
     console.log("printing user details");
-    console.log(userName + firstName + lastName + email + password + confirmPassword + role + dob);
+    console.log(userName + firstName + lastName + email + password + confirmPassword + role + phone + dob);
     return {
         username : userName,
         email : email,
@@ -53,16 +169,74 @@ function getUserDetailsFromForm() {
         password : password,
         confirmPassword : confirmPassword,
         role: role,
-        dob : dob
+        dob : dob,
+        phone: phone
     }
 
 }
+
+
+
+function checkIfEmailNotTaken(email) {
+    // return success if not taken (send request to server)
+    // $.ajax({
+    //     url: "/isEmailTaken",
+    //     data: {email: email},
+    //     type: "GET",
+    //
+    //     success: function (response) {
+    //         console.log("email -available");
+    //         emailValidated = true;
+    //         $("#emailAvailable").text("");
+    //         $("#emailAvailable").prepend('<img id="theImg" src="../templates/img/circleCheck.png" />');
+    //     },
+    //     error: function (xhr, status, error) {
+    //         console.log("in error of email");
+    //         $("#emailAvailable").text("email Taken");
+    //         $("#emailAvailable").css("color", "red");
+    //         emailValidated = false;
+    //     }
+    //
+    // });
+    $("#emailAvailable").text("valid");
+    $("#emailAvailable").css("color", "green");
+    return true
+
+}
+
+
+
+
 
 function main() {
     $('#navRegister').click(redirectToRegister);
     $('#navLogin').click(redirectToLogin);
     $('#register').click(register);
     $("#register").prop("disabled",true);
+    $('input').change(registerOnFormChange).keyup(registerOnFormChange);
+    $("#email").keyup(emailValidate).change(emailValidate);
+
+
+    $('#confirmPassword,#password').keyup(
+        function () {
+            if (isPasswordConfirmed())
+            {
+                // $('#message').html('<img id="passwordStatus" src="../templates/img/circleCheck.png" />')
+                console.log("password confirmed");
+
+            } else
+                // $('#message').html('<img id="passwordStatus" src="../templates/img/wrong.png" />')
+                console.log("Error: confirm  password not matching");
+
+        }
+    );
+
+
+    $('#password').keyup(isPasswordLengthValid);
+
+    $("#phoneNo").keyup(validatePhoneNumber).change(validatePhoneNumber);
+    $('#dob').keyup(validateDOB).change(validateDOB);
+
 
 }
 //IIFE
@@ -77,166 +251,3 @@ function main() {
 
 })();
 
-
-// ======================validation
-
-// function registerOnFormChange()
-// {
-//     const formValidated = ifFormValid();
-//     console.log(formValidated);
-//
-//     if (formValidated){
-//         $("#register").prop("disabled",false);
-//     }
-//     else{
-//         $("#register").prop("disabled",true);
-//     }
-//     return;
-//
-// }
-//
-// $(document).ready(function () {
-//
-//
-//     $('input').change(registerOnFormChange).keyup(registerOnFormChange);
-//
-//     $("#home").click(function(){redirectToWelcome();});
-//
-//     // checks bootstrap import
-//     var bootstrap_enabled = (typeof $().emulateTransitionEnd == 'function');
-//     console.log("bootstrap loaded : " + bootstrap_enabled);
-//
-//     $('#password_confirm,#password').keyup(
-//         function () {
-//             if (isPasswordConfirmed())
-//             {
-//                 $('#message').html('<img id="passwordStatus" src="../templates/img/circleCheck.png" />')
-//
-//             } else
-//                 $('#message').html('<img id="passwordStatus" src="../templates/img/wrong.png" />')
-//         }
-//     );
-//
-//     $("#email").keyup(emailValidate).change(emailValidate);
-//
-//     $('#password').keyup(isPasswordLengthValid);
-//
-//     // if password is valid and email is valid allow
-//     // else disable the register button
-//
-//     $("#register").click(function () {
-//
-//         var userDetails = {
-//             "userId"     :    $("#email").val(),
-//             "password"  :    $("#password").val(),
-//             "role"      :    $('input[name=role]:checked').val()
-//         }
-//
-//         $.ajax({
-//             type: "POST",
-//             url: "/userRegister",
-//             data: JSON.stringify(userDetails),
-//             contentType: 'application/json',
-//
-//             success: function (response) {
-//                 console.log("Success");
-//                 redirect1();
-//             },
-//             error: function (e) {
-//                 console.log('page not found' + e);
-//                 redirect2();
-//
-//             }
-//         });
-//     });
-//
-//     ifFormValid();
-//
-// });
-//
-// function ifFormValid() {
-//     return  emailValidated             &&
-//         isPasswordLengthValid()         &&
-//         isPasswordConfirmed();
-// }
-//
-// function emailValidate() {
-//     var $emailAvailable = $("#emailAvailable");
-//     var email = $("#email").val();
-//
-//     if (validateEmail(email))
-//     {
-//         emailValidated = true;
-//         console.log("existing email check");
-//         checkIfEmailNotTaken(email);
-//         registerOnFormChange()
-//         return emailValidated;
-//     }
-//     else {
-//
-//         $emailAvailable.text("invalid");
-//         $emailAvailable.css("color", "red");
-//         registerOnFormChange()
-//
-//         return false;
-//     }
-// }
-//
-// function validateEmail(email) {
-//     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(String(email).toLowerCase());
-// }
-//
-// function isPasswordConfirmed(){
-//
-//     return ($('#password').val() == $('#password_confirm').val() &&
-//         $('#password').val()!= ''); }
-//
-// function isPasswordLengthValid()
-// {
-//     if ($('#password').val().length < 4)
-//     {
-//         $('#passwordHelp').css("display", "block");
-//         return false;
-//     }
-//
-//     else
-//     {
-//         $('#passwordHelp').css("display", "none");
-//         return true;
-//     }
-// }
-//
-// function redirect1() {
-//
-//     console.log("in redirect");
-//     window.location = "../templates/userLogin.html";
-// }
-//
-// function redirect2() {
-//     window.location = "../templates/userRegister.html";
-// }
-//
-// function checkIfEmailNotTaken(email){
-//     // return success if not taken (send request to server)
-//     $.ajax({
-//         url : "/isEmailTaken",
-//         data: {email : email},
-//         type : "GET",
-//
-//         success : function(response) {
-//             console.log("email -available");
-//             emailValidated = true;
-//             $("#emailAvailable").text("");
-//             $("#emailAvailable").prepend('<img id="theImg" src="../templates/img/circleCheck.png" />');
-//         },
-//         error : function(xhr, status, error) {
-//             console.log("in error of email");
-//             $("#emailAvailable").text("email Taken");
-//             $("#emailAvailable").css("color", "red");
-//             emailValidated  = false;
-//         }
-//
-//     });
-//
-// };
