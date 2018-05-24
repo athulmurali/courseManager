6dabdecf5fb1d864b8fbbe38ec0d1bec361963a9
Athul Muralidharan <athulmurali@gmail.com>
2018-05-24 19:05:04 -0400

    Fixed emailConfig.properties - file not found error

--- src/main/java/com/example/neucourseManager/utilities/email/EMailConfigUtil.java
+++ src/main/java/com/example/neucourseManager/utilities/email/EMailConfigUtil.java
@@ -3,9 +3,12 @@ package com.example.neucourseManager.utilities.email;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 
@@ -33,12 +36,13 @@ public class EMailConfigUtil {
         logger.debug("Loading emailConfig.properties");
         Properties emailProp = new Properties();
 
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-
-        File file = new File(classloader.getResource(FILE_NAME).getFile());
-        FileInputStream fileInput = new FileInputStream(file);
-
-        emailProp.load(fileInput);
+        try {
+            File file = ResourceUtils.getFile("classpath:emailConfig.properties");
+            InputStream in = new FileInputStream(file);
+            emailProp.load(in);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
         return emailProp;
     }
 
