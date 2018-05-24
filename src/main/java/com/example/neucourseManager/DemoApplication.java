56681f6a66adbb725a04811cdfc39bd819a2f443
Athul Muralidharan <athulmurali@gmail.com>
2018-05-24 12:58:29 -0400

    Added main class

--- src/main/java/com/example/neucourseManager/DemoApplication.java
+++ src/main/java/com/example/neucourseManager/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.io.IOException;
 
 
 /**
@@ -18,7 +17,7 @@ import java.io.IOException;
 public class DemoApplication extends SpringBootServletInitializer {
 
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 
 		SpringApplication.run(DemoApplication.class, args);
 
